b15c5f99fb21dc4d1ee719eff14ed6bd1e74c6d6
Alexander Lazo <lazoalexander86@gmail.com>
2024-10-05 09:46:59 -0500

    hostErrorsSolutions4

--- src/firebase/credenciales.js
+++ src/firebase/credenciales.js
@@ -8,12 +8,10 @@ import { v4 } from "uuid";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_FIREBASE_APIKEY,
   authDomain: import.meta.env.VITE_APP_FIREBASE_AUTHDOMAIN ,
-  databaseURL: import.meta.env.VITE_APP_FIREBASE_DATABASEURL ,
   projectId: import.meta.env.VITE_APP_FIREBASE_PROJECTID ,
   storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGEBUCKET ,
   messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGINGSENDERID ,
   appId: import.meta.env.VITE_APP_FIREBASE_APPID ,
-  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENTID
 };
 
 // Inicializamos la aplicación y la guardamos en firebaseApp
