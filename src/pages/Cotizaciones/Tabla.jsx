7421c28a88c0e24d6f8ef71efad6a2b48e157135
Alexander Lazo <lazoalexander86@gmail.com>
2024-10-22 07:02:35 -0500

    overflow

--- src/pages/Cotizaciones/Tabla.jsx
+++ src/pages/Cotizaciones/Tabla.jsx
@@ -9,36 +9,38 @@ const CotizacionesTabla = ({ data }) => {
   */
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Nro Cotizacion</th>
-          <th>Fecha de Cotización</th>
-          <th>Fecha de Vencimiento</th>
-          <th>Codigo de Cliente</th>
-          <th>Nombre de Cliente</th>
-          <th>Monto Total</th>
-          {/* <th>Estado</th> */}
-          <th>Acciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <tr key={index}>
-            <td>{item.NumeroCotizacion}</td>
-            <td>{item.FechaEmision}</td>
-            <td>{item.FechaVencimiento}</td>
-            <td>{item.CodigoCli}</td>
-            <td>{item.Cliente.nombres}</td>
-            <td>{item.MontoTotal}</td>
-            {/* <td>{item.estado}</td> */}
-            <td>
-              <DetalleCotizacion cotizacion={item} />
-            </td>
+    <div style={{overflowY:"auto", height:"70vh", padding:"0px"}}>
+        <table  >
+        <thead>
+          <tr>
+            <th>Nro Cotizacion</th>
+            <th>Fecha de Cotización</th>
+            <th>Fecha de Vencimiento</th>
+            <th>Codigo de Cliente</th>
+            <th>Nombre de Cliente</th>
+            <th>Monto Total</th>
+            {/* <th>Estado</th> */}
+            <th>Acciones</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.NumeroCotizacion}</td>
+              <td>{item.FechaEmision}</td>
+              <td>{item.FechaVencimiento}</td>
+              <td>{item.CodigoCli}</td>
+              <td>{item.Cliente.nombres}</td>
+              <td>{item.MontoTotal}</td>
+              {/* <td>{item.estado}</td> */}
+              <td>
+                <DetalleCotizacion cotizacion={item} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
